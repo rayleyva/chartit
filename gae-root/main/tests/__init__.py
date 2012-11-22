4a798858e05876c8ea8cc1ce112cf0b1fceed878
Craig J Perry <craigp84@gmail.com>
2012-11-22 21:35:02 +0000

    Correct violations highlighted by static analyis tools.
    Small beer such as extra blank lines at the end of files.

--- gae-root/main/tests/__init__.py
+++ gae-root/main/tests/__init__.py
@@ -10,20 +10,11 @@ import os
 import sys
 
 
-def add_to_path(path):
-    """Add a dir relative to this file location, to the sys.path"""
-    this_dir = os.path.dirname(os.path.abspath(__file__))
-    sys.path.insert(0, os.path.join(this_dir, path))
-
-
 def setup():
     """Package level test fixture setup."""
     pass
-    #add_to_path('..')
-    #add_to_path(os.path.join('..', '..', 'lib'))
 
 
 def teardown():
     """Package level test fixture teardown."""
     pass
-
